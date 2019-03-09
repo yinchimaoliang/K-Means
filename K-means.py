b8e6cf6361519ce25f6f0a253e77ab6a0a7af39a
Yinhao Li <liyinhao0413@qq.com>
2019-03-09 19:14:24 +0800

    first commit

---
+++ K-means.py
@@ -0,0 +1,17 @@
+from numpy import *
+
+class Main():
+    def getData(self): #生成随机数作为数据集
+        self.data = random.rand(100,2) * 10
+        print(self.data)
+
+    def
+
+
+
+
+
+
+if __name__ == "__main__":
+    t = Main()
+    t.getData()
